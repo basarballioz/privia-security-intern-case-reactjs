48f2970caf69fc5c57ed88b4a256c8321e0b11b9
Başar Ballıöz <balliozbasar@gmail.com>
2021-05-22 22:18:21 +0300

    ilk section tamamlandi - responsive bekleniyor

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,7 +1,7 @@
 import React from 'react'
 
 function Home() {
-    
+
     return (
         <>
             <main>
@@ -9,6 +9,13 @@ function Home() {
                     <img src="/assets/svg/background.svg" className='home-background' alt="Privia Background Image" />
                     <h1>Powerfull analytics tools for your business</h1>
                     <p>An awesome tools for your business, increase business revenue with enterprise-grade links built to acquire and engage cutomers</p>
+                    <div className='trial-buttons'>
+                        <button className='primary'>Start free trial</button>
+                        <button>View live demo</button>
+                    </div>
+                    <div className='dashboard'>
+                        <img src="/assets/svg/dashboard.svg" alt="Privia Dashboard Image" />
+                    </div>
                 </section>
             </main>
         </>
