0b1b84bbe15c81c0f18fe34bbf92dd17c4c21bb1
Başar Ballıöz <balliozbasar@gmail.com>
2021-05-22 17:39:58 +0300

    router yapisi eklendi, gerekli scss'ler düzenlendi

--- src/App.js
+++ src/App.js
@@ -1,13 +1,24 @@
-import React from 'react';
+import React, { Suspense, lazy } from 'react'
 import './styles/index.scss'
+import Home from './pages/Home'
+import Header from './components/Header'
+import {BrowserRouter as Router, Switch, Route} from 'react-router-dom'
 
 function App() {
   return (
     <>
-      <main> ASDAS </main>
-      
+      <Suspense fallback={<div> Loading... </div>}>
+        <Header />
+        <main>
+          <Home />
+          
+        </main>
+
+      </Suspense>
+
     </>
   );
 }
 
 export default App;
+
--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,1 +1,34 @@
 import React from 'react'
+import styles from './Header.module.scss'
+import {Link} from 'react-router-dom'
+
+
+function Header() {
+
+    return (
+        <>
+            <header>
+                <div className={styles.logo}>
+                    <img src="/assets/svg/logo.svg" alt="Privia Logo" />
+                </div>
+                <nav>
+                    <ul>
+                        <li> <Link to='/about'> About </Link> </li>
+                        <li> <Link to='/product'> Product </Link> </li>
+                        <li> <Link to='/pricing'> Pricing </Link> </li>
+                        <li> <Link to='/resources'> Resources </Link> </li>
+                        <li> <Link to='/customers'> Customers </Link> </li>
+                    </ul>
+                </nav>
+
+                <div className="profile-area">
+                    <div className="login">Login</div>
+                    <div className="trial">Start Free Trial</div>
+                </div>
+
+            </header>
+        </>
+    )
+}
+
+export default Header
